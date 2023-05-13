c4a41083fa72f9c61a5529d6bf3210c198c94beb
aipilotbyjd <aicopilotideas@gmail.com>
2023-05-13 18:07:38 +0000

    wdw

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = next({ dev });
 
 app
@@ -11,7 +11,7 @@ app
   .then(() => {
     const server = express();
 
-    server.get("/", (req, res) => {
+    server.get("/api", (req, res) => {
       return res.send("Hello World");
     });
 

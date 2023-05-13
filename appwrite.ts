12655bb9301137438df2f37a319e91f9032c6927
aipilotbyjd <aicopilotideas@gmail.com>
2023-05-13 18:14:17 +0000

    DWD

--- appwrite.ts
+++ appwrite.ts
@@ -1,8 +1,23 @@
-const { Client } = require("node-appwrite");
+import { Client, Account } from "node-appwrite";
+
+interface IEnv {
+  APPWRITE_ENDPOINT: string;
+  APPWRITE_PROJECT: string;
+  APPWRITE_API_KEY: string;
+}
+
+declare var process: {
+  env: IEnv;
+};
 
 const client = new Client()
   .setEndpoint(process.env.APPWRITE_ENDPOINT) // Your API Endpoint
   .setProject(process.env.APPWRITE_PROJECT) // Your project ID
   .setKey(process.env.APPWRITE_API_KEY); // Your secret API key
 
-module.exports = client;
+const account = new Account(client);
+
+module.exports = {
+  client,
+  account,
+};

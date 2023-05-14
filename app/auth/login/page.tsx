26f42b421f1207f14f3f5b1a52ed478b8abf7019
aipilotbyjd <aicopilotideas@gmail.com>
2023-05-14 12:05:18 +0000

    wsw

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -8,7 +8,7 @@ const Login = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    fetch(`${apibaseurl}/api/auth/signup`, {
+    fetch(`${apibaseurl}/api/auth/login`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),

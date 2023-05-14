bea22c703aabfdfe26c5991edd9e6d7bb88a49ea
aipilotbyjd <aicopilotideas@gmail.com>
2023-05-14 11:55:43 +0000

    sws

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const next = require("next");
-const { SignUp } = require("../controllers/auth");
+const { SignUp, Login } = require("../controllers/auth");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
